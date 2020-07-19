c2bca26429436d2b5650a85cc39037c514ca8b31
fanliu <fan.liu@rutgers.edu>
2020-07-18 22:17:34 -0400

    segment function

--- segment.py
+++ segment.py
@@ -21,3 +21,23 @@ def run_avg(image, aWeight):
         return
 
     cv2.accumulateWeighted(image, bg, aWeight)
+
+
+def segment(image, threshold=25):
+    global bg
+    # find the absolute difference between background and current frame
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+
+    # threshold the difference image so that we get the foreground
+    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+
+    # get the contours in the thresholded image
+    (_, cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # return None if no contours detected
+    if len(cnts) == 0:
+        return
+    else:
+        # based on contour area, get the maximum contour which is the hand
+        segmented = max(cnts, key=cv2.contourArea)
+        return thresholded, segmented
